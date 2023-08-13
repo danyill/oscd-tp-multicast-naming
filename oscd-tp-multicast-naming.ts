7a5075693a267b87e82bd0de1477cecfef8abd8c
Daniel Mulholland <dan.mulholland@gmail.com>
2023-08-14 07:07:16 +1200

    wip

--- oscd-tp-multicast-naming.ts
+++ oscd-tp-multicast-naming.ts
@@ -1,16 +1,27 @@
 /* eslint-disable no-return-assign */
-import { css, html, LitElement, TemplateResult } from 'lit';
+import { css, html, LitElement, PropertyValueMap, TemplateResult } from 'lit';
 import { property, query } from 'lit/decorators.js';
 
 import { Dialog } from '@material/mwc-dialog';
 
-import '@material/mwc-formfield';
 import '@material/mwc-button';
+
+import '@material/mwc-checkbox';
+import '@material/mwc-formfield';
 import '@material/mwc-icon-button';
-import '@openscd/oscd-filtered-list';
 import '@material/mwc-list/mwc-list-item';
 
+import '@vaadin/grid';
+import '@vaadin/checkbox';
+
+import '@vaadin/grid/theme/material/vaadin-grid.js';
+import '@vaadin/grid/theme/material/vaadin-grid-filter-column.js';
+import '@vaadin/grid/theme/material/vaadin-grid-selection-column.js';
+
+import { columnBodyRenderer, columnHeaderRenderer } from '@vaadin/grid/lit.js';
+
 import type { List, MWCListIndex } from '@material/mwc-list';
+import type { Grid } from '@vaaadin/grid';
 import { Edit, newEditEvent } from '@openscd/open-scd-core';
 
 import { identity } from './foundation/identities/identity.js';
@@ -185,6 +196,17 @@ function translate(str: string): string {
   return str;
 }
 
+function getCommAddress(ctrlBlock: Element): Element {
+  const doc = ctrlBlock.ownerDocument;
+
+  const ctrlLdInst = ctrlBlock.closest('LDevice')!.getAttribute('inst');
+  const addressTag = ctrlBlock.tagName === 'GSEControl' ? 'GSE' : 'SMV';
+  const cbName = ctrlBlock.getAttribute('name');
+  return doc.querySelector(
+    `${addressTag}[ldInst="${ctrlLdInst}"][cbName="${cbName}"]`
+  )!;
+}
+
 export default class TPMulticastNaming extends LitElement {
   /** The document being edited as provided to plugins by [[`OpenSCD`]]. */
   @property({ attribute: false })
@@ -196,8 +218,14 @@ export default class TPMulticastNaming extends LitElement {
   @property({ attribute: false })
   editCount!: number;
 
-  @query('mwc-dialog')
-  dialog!: Dialog;
+  @query('#grid')
+  grid!: Grid;
+
+  @property()
+  gridItems: Array<unknown> = [];
+
+  @property()
+  selectedItems: Array<unknown> = [];
 
   @property()
   publisherGOOSE = true;
@@ -220,12 +248,12 @@ export default class TPMulticastNaming extends LitElement {
   @query('.selection-list')
   cbList: List | undefined;
 
-  async run(): Promise<void> {
-    this.dialog.open = true;
-    this.cbList?.addEventListener('selected', () => {
-      this.selectedControlItems = this.cbList!.index;
-    });
-  }
+  // async run(): Promise<void> {
+  //   this.dialog.open = true;
+  //   this.cbList?.addEventListener('selected', () => {
+  //     this.selectedControlItems = this.cbList!.index;
+  //   });
+  // }
 
   // eslint-disable-next-line class-methods-use-this
   private onClosed(ae: CustomEvent<{ action: string } | null>): void {
@@ -239,19 +267,39 @@ export default class TPMulticastNaming extends LitElement {
         ><mwc-checkbox
           value="GOOSE"
           ?checked=${this.publisherGOOSE}
-          @change=${() => (this.publisherGOOSE = !this.publisherGOOSE)}
+          @change=${() => {
+            this.publisherGOOSE = !this.publisherGOOSE;
+            this.gridItems = [];
+            this.grid.clearCache();
+            this.updateContent();
+            this.grid.requestContentUpdate();
+          }}
         ></mwc-checkbox></mwc-formfield
       ><mwc-formfield label="Sampled Value"
         ><mwc-checkbox
           value="SampledValue"
           ?checked=${this.publisherSMV}
-          @change=${() => (this.publisherSMV = !this.publisherSMV)}
+          @change=${() => {
+            this.publisherSMV = !this.publisherSMV;
+            this.gridItems = [];
+            this.updateContent();
+            this.grid.clearCache();
+            this.grid.requestContentUpdate();
+            // this.requestUpdate();
+          }}
         ></mwc-checkbox
       ></mwc-formfield>
       <mwc-formfield label="Prot 1"
         ><mwc-checkbox
           ?checked=${this.protection1}
-          @change=${() => (this.protection1 = !this.protection1)}
+          @change=${() => {
+            this.protection1 = !this.protection1;
+            this.gridItems = [];
+            this.updateContent();
+            this.grid.clearCache();
+            this.grid.requestContentUpdate();
+            // this.requestUpdate();
+          }}
         ></mwc-checkbox
       ></mwc-formfield>
       <mwc-formfield label="Prot 2"
@@ -263,84 +311,235 @@ export default class TPMulticastNaming extends LitElement {
     </div>`;
   }
 
-  renderSelectionList(): TemplateResult {
-    if (!this.doc) return html``;
-    const noSelectedComms =
-      this.publisherGOOSE === false && this.publisherSMV === false;
+  protected updateContent(): void {
+    if (!this.doc) return;
 
-    const selectorString = selectControlBlockTypes(
-      this.publisherGOOSE,
-      this.publisherSMV
-    );
-    const protectionSelection = `${this.protection1 ? '1' : ''}${
+    const protections = `${this.protection1 ? '1' : ''}${
       this.protection2 ? '2' : ''
     }`;
-    this.commElements = [];
-    return html`<oscd-filtered-list multi class="selection-list"
-      >${Array.from(
-        noSelectedComms
-          ? this.doc.querySelectorAll('XYZZY')
-          : this.doc.querySelectorAll('ConnectedAP')
-      )
-        .filter(ap => ap.querySelector(selectorString) !== null)
-        .filter(ap =>
-          selectProtections(ap.getAttribute('iedName')!, protectionSelection)
-        )
-        .sort(compareNames)
-        .flatMap(ap => {
-          const apItem = html`<mwc-list-item
-              class="listitem header"
-              noninteractive
-              graphic="icon"
-              value="${Array.from(ap.querySelectorAll(selectorString))
-                .map(comm => {
-                  const id = `${identity(comm)}` as string;
-                  return typeof id === 'string' ? id : '';
-                })
-                .join(' ')}"
-            >
-              <span>${ap.getAttribute('iedName')} > ${ap.getAttribute(
-            'apName'
-          )}</span>
-              <mwc-icon slot="graphic">developer_board</mwc-icon>
-            </mwc-icon-button>
-            </mwc-list-item>
-            <li divider role="separator"></li>`;
 
-          const currentComElements = Array.from(
-            ap.querySelectorAll(selectorString)
-          );
-          this.commElements = [...this.commElements, ...currentComElements];
-          const commUiElements = currentComElements.map(
-            comm =>
-              html`<mwc-check-list-item
-                hasMeta
-                twoline
-                value="${comm.getAttribute(
-                  'cbName'
-                )} ${comm.parentElement!.getAttribute(
-                  'iedName'
-                )} ${comm.parentElement!.getAttribute('apName')} "
-                graphic="icon"
-              >
-                <span>${comm.getAttribute('cbName')}</span
-                ><span slot="secondary"
-                  >${(<string>identity(comm))
-                    .split(' ')
-                    .slice(0, -1)
-                    .join('')}</span
-                >
-                <mwc-icon slot="graphic"
-                  >${comm.tagName === 'GSE' ? gooseIcon : smvIcon}</mwc-icon
-                >
-                <mwc-icon-button slot="meta" icon="edit"></mwc-icon-button>
-              </mwc-check-list-item>`
-          );
+    Array.from(this.doc.querySelectorAll('GSEControl, SampledValueControl'))
+      .filter(control => {
+        const iedName =
+          control.closest('IED')?.getAttribute('name') ?? 'Unknown IED';
+        console.log(
+          protections.includes(getProtectionNumber(iedName)),
+          iedName,
+          protections
+        );
+        return (
+          ((control.tagName === 'GSEControl' && this.publisherGOOSE) ||
+            (control.tagName === 'SampledValueControl' && this.publisherSMV)) &&
+          protections.includes(getProtectionNumber(iedName))
+        );
+      })
+      .forEach(control => {
+        const address = getCommAddress(control);
+        const ied = control.closest('IED');
 
-          return [apItem, ...commUiElements];
-        })}</oscd-filtered-list
-    >`;
+        const rowItem = {
+          iedName: ied!.getAttribute('name')!,
+          iedType: ied!.getAttribute('type')!,
+          type: address?.tagName,
+          cbName: control.getAttribute('name'),
+          appOrSmvId:
+            control.tagName === 'GSEControl'
+              ? control.getAttribute('appID') ?? ''
+              : control.getAttribute('smvID') ?? '',
+          apName: address?.closest('ConnectedAP')?.getAttribute('apName') ?? '',
+          mac:
+            address?.querySelector('Address > P[type="MAC-Address"]')
+              ?.textContent ?? '',
+          appId:
+            address?.querySelector('Address > P[type="APPID"]')?.textContent ??
+            '',
+          vlanId:
+            address?.querySelector('Address > P[type="VLAN-ID"]')
+              ?.textContent ?? '',
+          vlanPriority:
+            address?.querySelector('Address > P[type="VLAN-PRIORITY"]')
+              ?.textContent ?? '',
+          minTime: address?.querySelector('MinTime')?.textContent ?? '',
+          maxTime: address?.querySelector('MaxTime')?.textContent ?? '',
+        };
+
+        // if (this.grid) {
+        if (this.gridItems) {
+          this.gridItems.push({ ...rowItem });
+        } else {
+          this.gridItems = [{ ...rowItem }];
+        }
+        // }
+      });
+    console.log(this.gridItems);
   }
+
+  protected updated(
+    _changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>
+  ): void {
+    // this.gridItems = [];
+    // this.updateContent();
+  }
+
+  async firstUpdated(): Promise<void> {
+    // if (!this.doc) return;
+    // ${Array.from(
+    //   noSelectedComms
+    //     ? this.doc.querySelectorAll('XYZZY')
+    //     : this.doc.querySelectorAll('ConnectedAP')
+    // )
+    //   .filter(ap => ap.querySelector(selectorString) !== null)
+    //   .filter(ap =>
+    //     selectProtections(ap.getAttribute('iedName')!, protectionSelection)
+    //   )
+    //   .sort(compareNames)
+    //   .flatMap(ap => {
+    //     const apItem = {
+    //       name: ap.getAttribute('iedName'),
+    //       apName: ap.getAttribute('apName'),
+    //     };
+    //     const currentComElements = Array.from(
+    //       ap.querySelectorAll(selectorString)
+    //     );
+    //     this.commElements = [...this.commElements, ...currentComElements];
+    // const commUiElements = currentComElements.map(
+    //   comm =>
+    //     html`<mwc-check-list-item
+    //       hasMeta
+    //       twoline
+    //       value="${comm.getAttribute(
+    //         'cbName'
+    //       )} ${comm.parentElement!.getAttribute(
+    //         'iedName'
+    //       )} ${comm.parentElement!.getAttribute('apName')} "
+    //       graphic="icon"
+    //     >
+    //       <span>${comm.getAttribute('cbName')}</span
+    //       ><span slot="secondary"
+    //         >${(<string>identity(comm))
+    //           .split(' ')
+    //           .slice(0, -1)
+    //           .join('')}</span
+    //       >
+    //       <mwc-icon slot="graphic"
+    //         >${comm.tagName === 'GSE' ? gooseIcon : smvIcon}</mwc-icon
+    //       >
+    //       <mwc-icon-button slot="meta" icon="edit"></mwc-icon-button>
+    //     </mwc-check-list-item>`
+    // );
+    // return [apItem, ...commUiElements];
+    // })}
+    //   const noSelectedComms =
+    //   this.publisherGOOSE === false && this.publisherSMV === false;
+    // const selectorString = selectControlBlockTypes(
+    //   this.publisherGOOSE,
+    //   this.publisherSMV
+    // );
+    // const protectionSelection = `${this.protection1 ? '1' : ''}${
+    //   this.protection2 ? '2' : ''
+    // }`;
+    // this.commElements = [];
+    // this.updateContent();
+  }
+
+  renderSelectionList(): TemplateResult {
+    if (!this.doc) return html``;
+
+    if (!(this.gridItems.length > 0)) this.updateContent();
+    // frozen
+    return html`
+      <vaadin-grid
+        id="grid"
+        column-reordering-allowed
+        overflow="bottom"
+        .items=${this.gridItems}
+      >
+        <vaadin-grid-selection-column
+          ${columnHeaderRenderer(
+            () =>
+              html`<mwc-checkbox
+                ?indeterminate=${this.grid.selectedItems.length > 1 &&
+                this.grid.selectedItems.length !== this.gridItems.length}
+                ?checked=${this.grid.selectedItems.length ===
+                this.gridItems.length}
+              ></mwc-checkbox>`,
+            []
+          )}
+          ${columnBodyRenderer<any>(
+            ({ selected }) =>
+              html`<mwc-checkbox ?checked=${selected}></mwc-checkbox>`,
+            []
+          )}
+        ></vaadin-grid-selection-column>
+        <vaadin-grid-filter-column
+          path="iedName"
+          header="IED Name"
+        ></vaadin-grid-filter-column>
+        <vaadin-grid-filter-column
+          path="iedType"
+          header="IED Type"
+        ></vaadin-grid-filter-column>
+        <vaadin-grid-filter-column
+          path="type"
+          header="Type"
+        ></vaadin-grid-filter-column>
+        <vaadin-grid-filter-column
+          id="cb"
+          path="cbName"
+          header="Control Name"
+        ></vaadin-grid-filter-column>
+        <vaadin-grid-filter-column
+          id="cb"
+          path="appOrSmvId"
+          header="App or SMV Id"
+        ></vaadin-grid-filter-column>
+        <vaadin-grid-filter-column
+          id="ap"
+          path="apName"
+          header="apName"
+        ></vaadin-grid-filter-column>
+        <vaadin-grid-filter-column
+          ${columnBodyRenderer<any>(
+            ({ mac }) =>
+              html`<span class="lighter">${(<string>mac).slice(0, 12)}</span
+                ><span>${mac.slice(12)}</span>`,
+            []
+          )}
+          id="mac"
+          path="mac"
+          header="mac"
+        ></vaadin-grid-filter-column>
+        <vaadin-grid-filter-column
+          id="appId"
+          path="appId"
+          header="APP ID"
+        ></vaadin-grid-filter-column>
+        <vaadin-grid-filter-column
+          id="vlanId"
+          path="vlanId"
+          header="vlanId"
+        ></vaadin-grid-filter-column>
+        <vaadin-grid-filter-column
+          id="vlanPriority"
+          path="vlanPriority"
+          header="vlanPriority"
+        ></vaadin-grid-filter-column>
+        <vaadin-grid-filter-column
+          id="minTime"
+          path="minTime"
+          header="minTime"
+        ></vaadin-grid-filter-column>
+        <vaadin-grid-filter-column
+          id="maxTime"
+          path="maxTime"
+          header="maxTime"
+        ></vaadin-grid-filter-column>
+      </vaadin-grid>
+    `;
+  }
+
+  // <!-- .renderer="${this.doDataSetRendering}" -->
+  // https://github.com/openscd/open-scd/pull/568/commits/056ef7424a2503dcff948e6f65497069f29d4107
 
   updateCommElements(selectedCommElements: Element[]): void {
     // MAC Addresses
@@ -482,29 +681,22 @@ export default class TPMulticastNaming extends LitElement {
   renderButtons(): TemplateResult {
     const sizeSelectedItems = (<Set<number>>this.selectedControlItems).size;
     return html`<mwc-button
-        outlined
-        icon="drive_file_rename_outline"
-        class="rename-button"
-        label="Rename GOOSE and SMV (${sizeSelectedItems || '0'})"
-        slot="primaryAction"
-        ?disabled=${(<Set<number>>this.selectedControlItems).size === 0 ||
-        (Array.isArray(this.selectedControlItems) &&
-          !this.selectedControlItems.length)}
-        @click=${() => {
-          const selectedCommElements = Array.from(
-            (<Set<number>>this.selectedControlItems).values()
-          ).map(index => this.commElements[index]);
-          this.updateCommElements(selectedCommElements);
-          this.dialog.open = false;
-        }}
-      >
-      </mwc-button>
-      <mwc-button
-        slot="secondaryAction"
-        dialogAction="close"
-        label="${translate('close')}"
-        style="--mdc-theme-primary: var(--mdc-theme-error)"
-      ></mwc-button>`;
+      outlined
+      icon="drive_file_rename_outline"
+      class="rename-button"
+      label="Rename GOOSE and SMV (${sizeSelectedItems || '0'})"
+      slot="primaryAction"
+      ?disabled=${(<Set<number>>this.selectedControlItems).size === 0 ||
+      (Array.isArray(this.selectedControlItems) &&
+        !this.selectedControlItems.length)}
+      @click=${() => {
+        const selectedCommElements = Array.from(
+          (<Set<number>>this.selectedControlItems).values()
+        ).map(index => this.commElements[index]);
+        this.updateCommElements(selectedCommElements);
+      }}
+    >
+    </mwc-button>`;
   }
 
   render(): TemplateResult {
@@ -515,54 +707,41 @@ export default class TPMulticastNaming extends LitElement {
   }
 
   static styles = css`
-    mwc-dialog {
-      --mdc-dialog-max-height: 80vh;
+    #grid {
+      width: auto;
+      margin: 1rem;
+      margin-right: 2rem;
+      height: 400px;
     }
 
-    .publishertypeselector {
-      margin: 4px 8px 8px;
-      background-color: var(--mdc-theme-surface);
-      width: calc(100% - 16px);
-      justify-content: space-around;
+    vaadin-grid::part(header-cell) {
+      background-color: white;
     }
 
-    .content {
-      max-width: 800px;
-      max-height: 60vh;
+    .lighter {
+      font-weight: lighter;
+      color: darkgray;
     }
 
-    .selectionlist {
-      flex: 35%;
-      margin: 4px 4px 4px 8px;
-      background-color: var(--mdc-theme-surface);
-      overflow-y: scroll;
+    /* vaadin-grid > vaadin-checkbox {
+
     }
 
-    .listitem.header {
-      font-weight: 500;
-    }
+    vaadin-grid > #selectAllCheckbox {
 
-    @media (max-width: 599px) {
-      .content {
-        height: 100%;
-      }
+    } */
 
-      .selectionlist {
-        position: absolute;
-        width: calc(100% - 32px);
-        height: auto;
-        top: 110px;
-        left: 8px;
-        background-color: var(--mdc-theme-surface);
-        z-index: 1;
-        box-shadow: 0 8px 10px 1px rgba(0, 0, 0, 0.14),
-          0 3px 14px 2px rgba(0, 0, 0, 0.12), 0 5px 5px -3px rgba(0, 0, 0, 0.2);
-      }
+    /* matching vaadin-grid material themes with open-scd themes */
+    * {
+      --material-body-text-color: var(--base00);
+      --material-secondary-text-color: var(--base00);
+      --material-primary-text-color: var(--base01);
+      --material-error-text-color: var(--red);
+      --material-primary-color: var(--primary);
+      --material-error-color: var(--red);
+      --material-background-color: var(--base3);
 
-      mwc-button {
-        display: flex;
-        margin: 4px 8px 8px;
-      }
+      --lumo-primary-color: var(--primary, blue);
     }
   `;
 }
