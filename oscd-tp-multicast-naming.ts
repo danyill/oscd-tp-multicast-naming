7f920671f127234c5ffe66e8ddc0131cd4570613
Jarrad Raumati <jarrad.raumati@gmail.com>
2023-12-19 15:59:28 +1300

    fix(SMV): corrected VTSelStn SMVid check
    - Closes #9

--- oscd-tp-multicast-naming.ts
+++ oscd-tp-multicast-naming.ts
@@ -1183,7 +1183,7 @@ export default class TPMulticastNaming extends LitElement {
         smvIDFunction = control
           .getAttribute('smvID')
           ?.replace(`${iedName}`, '')
-          .replace('/', '');
+          .replace('_', '');
 
       const controlName = control.getAttribute('name')!;
 
