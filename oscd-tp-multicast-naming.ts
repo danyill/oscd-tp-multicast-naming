d765e03e5c1b5acbd096da7bd9ae3f30e2400548
danyill <danyill@users.noreply.github.com>
2024-07-15 14:35:06 +1200

    Update oscd-tp-multicast-naming.ts

--- oscd-tp-multicast-naming.ts
+++ oscd-tp-multicast-naming.ts
@@ -261,7 +261,7 @@ function getAllocatedVlans(doc: XMLDocument): AllocatedVlans {
 const vlanRanges = {
   Station: {
     InterProt: { min: 1050, max: 1059, offsetToP2: 0 },
-    GSE: { min: 1000, max: 1049, offsetToP2: 1000 },
+    GSE: { min: 1001, max: 1049, offsetToP2: 1000 },
     SMV: { min: 1060, max: 1099, offsetToP2: 1000 },
   },
   Bus: {
